d15e34b450fe899e857d84f94f27122dbb7bcb9c
Mohammad Ghaith Albaba <mohammadalbaba491@gmail.com>
2020-06-24 14:25:56 +0200

    design the app bar
    Co-Authored-By: ma7amad4088 <mohammadalbaba491@gmail.com>

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -7,7 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sensor Display',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Display Sensor'),
+          backgroundColor: Color.fromRGBO(111, 255, 194, 1),
+          elevation: 6,
+          leading: IconButton(
+              icon: new Image.asset('assets/icons/icons8-iot-sensor-100.png'),
+              onPressed: null),
+          titleSpacing: 5,
+        ),
+      ),
+    );
   }
 }
-
