b9f61ab18fecafbc4c98ec505ceb608beb959e6e
Mohammad Ghaith Albaba <mohammadalbaba491@gmail.com>
2020-09-16 04:14:01 +0200

    Abgabe

--- arduino/DHT_MQTT/DHT_MQTT.ino
+++ arduino/DHT_MQTT/DHT_MQTT.ino
@@ -1,28 +1,47 @@
+// https://arduinojson.org/v6/doc/ zur ArduinoJson6 Documentation
 #include <ArduinoJson.h>
+
 #include <ESP8266WiFi.h>
+
+// https://github.com/256dpi/arduino-mqtt zur arduino-mqtt Documentation
 #include <MQTT.h>
 
+// http://arduino.cc/playground/Main/DHTLib zur DHTxx sensors Documentation
 #include "dht.h"
 
+// definieren vom pin, an dem das DHT Sensor verbunden wird.
 #define dht_apin D5
+
+// eine Instanz von DHT Klass
 dht DHT;
 
+// WIFI info
 const char ssid[] = "Daba";
 const char pass[] = "27408900";
 
+// eine Instanz von WiFiClient Klass
 WiFiClient net;
+
+// eine Instanz von MQTTClient Klass, um die Verbindung zwischen
+//dem Microkontroller und MQTT Broker zu verwalten
 MQTTClient client;
 
 unsigned long lastMillis = 0;
 
 void connect() {
+  
+  // warten bis der Microkontroller mit dem Netz verbindent.
   Serial.print("checking wifi...");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
 
+  // warten bis der Microkontroller mit dem MQTT Broker verbindet.
   Serial.print("\nconnecting...");
+  // <clientId-AYeFH92p3c> ist die Client-id und kann geändert werden.
+  // <hs-anhalt-ims> ist der Username und kann geändert werden.
+  // <hs-anhalt-ims> ist das Passwort und kann geändert werden.
   while (!client.connect("clientId-AYeFH92p3c", "hs-anhalt-ims", "hs-anhalt-ims")) {
     Serial.print(".");
     delay(1000);
@@ -30,47 +49,67 @@ void connect() {
 
   Serial.println("\nconnected!");
 
+  // Der Client abonniert das Topic z.B. </ledwemos1>,
+  // an dem die Messwerte geschickt werden, und kann geändert werden.
   client.subscribe("/ledwemos1");
-  // client.unsubscribe("/hello");
+  // client.unsubscribe("/ledwemos1");
 }
 
+// Diese Funktion behandelt die Nachrichten, die an dem genannten topic geschickt wurden.
 void messageReceived(String &topic, String &payload) {
   //Serial.println("incoming: " + topic + " - " + payload);
+  
+  // Logik, um die LED ein- und auszuschalten
+  // Diese Logik kann für anderes Fall gelöscht oder geändert werden
   if(payload=="true"){
     digitalWrite(LED_BUILTIN, HIGH);
   }
   else if (payload=="false"){
     digitalWrite(LED_BUILTIN, LOW);
   }
+  
 }
 
 void setup() {
+  // Die Ausgabe Modus wurde der LED zugeordnet.
   pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(115200);
+  
+  // Hier wird die Verbindung zwischen dem Microkontroller und WIFI vorbereitet.
   WiFi.begin(ssid, pass);
-
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
+  
+  // Hier wird die Verbindung zwischen dem Microkontroller und MQTT Broker vorbereitet.
   client.begin("broker.mqttdashboard.com", net);
-  client.onMessage(messageReceived);
 
+  // Hier wird die Funktion difiniert, die wenn eine Nachricht emfangen wurde.
+  client.onMessage(messageReceived);
+  
   connect();
 }
 
 void loop() {
   client.loop();
   delay(10);  // <- fixes some issues with WiFi stability
+  
+  // Hier wird festgestellt, an welechem Pin gehört, 
+  // um Messdaten des DHT Sensors zu empfangen.
   DHT.read11(dht_apin);
+
+  // Die gemessenen Werte des DHT Sensor werden in Variabeln gesetzt.
   float temp = DHT.temperature;
   float humi = DHT.humidity;
   
+  // Sicherstellen, dass MQTT client bereit ist. 
   if (!client.connected()) {
     connect();
   }
+  
   // publish a message roughly every second.
   if (millis() - lastMillis > 1000) {
     lastMillis = millis();
-    
+
+    // https://arduinojson.org/v6/doc/ zur ArduinoJson6 Documentation
     const size_t CAPACITY = JSON_OBJECT_SIZE(4);
     StaticJsonDocument<CAPACITY> doc;
     JsonObject object = doc.to<JsonObject>();
@@ -79,7 +118,9 @@ void loop() {
     String humis=String(humi);
     object["temp"] = temps;
     object["humidity"] = humis;
-    serializeJson(doc, message);  
+    serializeJson(doc, message);
+    
+    // Die Messwerte werden als Json Objekt an dem genannten Topic durch MQTT geschickt.   
     client.publish("/infowemos1", message);
   }
   delay(200);
---
+++ arduino/DHT_MQTT_2/DHT_MQTT_2.ino
@@ -0,0 +1,104 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+
+#include "dht.h"
+
+#define dht_apin D5
+
+#include <LiquidCrystal_I2C.h>
+
+
+dht DHT;
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE);
+     //Das Wort piezo ist jetzt die Zahl 5 
+
+const char ssid[] = "Taem";
+const char pass[] = "taemalJAZZAR123**";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("clientId-AYeFH92p3c", "hs-anhalt-ims", "hs-anhalt-ims")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/ledwemos1");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  //Serial.println("incoming: " + topic + " - " + payload);
+  if(payload=="true"){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else if (payload=="false"){
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  lcd.begin(16,2);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("broker.mqttdashboard.com", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+  DHT.read11(dht_apin);
+  
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+
+  float temp = DHT.temperature;
+  float humi = DHT.humidity;
+
+  lcd.print("Humidity");
+  lcd.print(humi);
+
+  lcd.setCursor(0,1);
+  lcd.print("temperature");
+  lcd.print(temp);
+  
+  if (!client.connected()) {
+    connect();
+  }
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    
+    const size_t CAPACITY = JSON_OBJECT_SIZE(4);
+    StaticJsonDocument<CAPACITY> doc;
+    JsonObject object = doc.to<JsonObject>();
+    String message;
+    String temps=String(temp);
+    String humis=String(humi);
+    object["temp"] = temps;
+    object["humidity"] = humis;
+    serializeJson(doc, message);  
+    client.publish("/infowemos1", message);
+  }
+  delay(200);
+}
